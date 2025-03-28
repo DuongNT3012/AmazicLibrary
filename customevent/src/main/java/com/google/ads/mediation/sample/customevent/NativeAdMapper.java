a2c54c78cd7b7005230ae250706af3819d6ad4f6
duongnt1 <@123456>
2025-03-28 18:05:21 +0700

    add custom event adx 28/03/2025

---
+++ customevent/src/main/java/com/google/ads/mediation/sample/customevent/NativeAdMapper.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2015 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.ads.mediation.sample.customevent;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+
+import com.google.ads.mediation.sample.sdk.SampleNativeAd;
+import com.google.android.gms.ads.mediation.MediationNativeAdCallback;
+import com.google.android.gms.ads.nativead.NativeAd;
+
+import java.text.NumberFormat;
+import java.util.Map;
+
+/**
+ * A {@link com.google.android.gms.ads.mediation.NativeAdMapper} extension to map {@link SampleNativeAd} instances to the Mobile
+ * Ads SDK's {@link com.google.android.gms.ads.nativead.NativeAd} interface.
+ */
+public class NativeAdMapper extends com.google.android.gms.ads.mediation.NativeAdMapper {
+
+    private final NativeAd nativeAd;
+    private MediationNativeAdCallback mediationNativeAdCallback;
+    public void setMediationNativeAdCallback(MediationNativeAdCallback mediationNativeAdCallback) {
+        this.mediationNativeAdCallback = mediationNativeAdCallback;
+    }
+
+    public NativeAdMapper(@NonNull NativeAd nativeAd, Context context) {
+        this.nativeAd = nativeAd;
+        if (nativeAd.getHeadline() != null)
+            setHeadline(nativeAd.getHeadline());
+        if (nativeAd.getBody() != null)
+            setBody(nativeAd.getBody());
+        if (nativeAd.getCallToAction() != null)
+            setCallToAction(nativeAd.getCallToAction());
+        if (nativeAd.getStarRating() != null)
+            setStarRating(nativeAd.getStarRating());
+        if (nativeAd.getStore() != null)
+            setStore(nativeAd.getStore());
+        if (nativeAd.getIcon() != null)
+            setIcon(nativeAd.getIcon());
+        if (nativeAd.getAdvertiser() != null)
+            setAdvertiser(nativeAd.getAdvertiser());
+        setImages(nativeAd.getImages());
+
+        if (nativeAd.getPrice() != null) {
+            NumberFormat formatter = NumberFormat.getCurrencyInstance();
+            String priceString = formatter.format(nativeAd.getPrice());
+            setPrice(priceString);
+        }
+
+        /*Bundle extras = new Bundle();
+        extras.putString(CustomEvent.DEGREE_OF_AWESOMENESS, ad.getDegreeOfAwesomeness());
+        this.setExtras(extras);*/
+
+        setOverrideClickHandling(false);
+        setOverrideImpressionRecording(false);
+
+        try {
+            if (nativeAd.getAdChoicesInfo() != null) {
+                NativeAd.Image adChoicesImage = nativeAd.getAdChoicesInfo().getImages().get(0);
+                ImageView adChoicesImageView = new ImageView(context);
+                adChoicesImageView.setImageDrawable(adChoicesImage.getDrawable());
+                setAdChoicesContent(adChoicesImageView);
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public void recordImpression() {
+        this.mediationNativeAdCallback.reportAdImpression();
+    }
+
+    @Override
+    public void handleClick(@NonNull View view) {
+        this.mediationNativeAdCallback.reportAdClicked();
+    }
+
+    // The Sample SDK doesn't do its own impression/click tracking, instead relies on its
+    // publishers calling the recordImpression and handleClick methods on its native ad object. So
+    // there's no need to pass it a reference to the View being used to display the native ad. If
+    // your mediated network does need a reference to the view, the following method can be used
+    // to provide one.
+    @Override
+    public void trackViews(@NonNull View containerView,
+                           @NonNull Map<String, View> clickableAssetViews,
+                           @NonNull Map<String, View> nonClickableAssetViews) {
+        super.trackViews(containerView, clickableAssetViews, nonClickableAssetViews);
+        // If your ad network SDK does its own impression tracking, here is where you can track the
+        // top level native ad view and its individual asset views.
+    }
+
+    @Override
+    public void untrackView(@NonNull View view) {
+        super.untrackView(view);
+        // Here you would remove any trackers from the View added in trackView.
+    }
+}
---
+++ customevent/src/main/java/com/google/ads/mediation/sample/customevent/NativeCustomEventLoader.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2015 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.ads.mediation.sample.customevent;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.ads.mediation.sample.customevent.util.FirebaseAnalyticsUtil;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.VideoOptions;
+import com.google.android.gms.ads.mediation.MediationAdLoadCallback;
+import com.google.android.gms.ads.mediation.MediationNativeAdCallback;
+import com.google.android.gms.ads.mediation.MediationNativeAdConfiguration;
+import com.google.android.gms.ads.nativead.NativeAdOptions;
+
+/**
+ * Native custom event loader for the SampleSDK.
+ */
+public class NativeCustomEventLoader {
+
+    /**
+     * Configuration for requesting the native ad from the third party network.
+     */
+    private final MediationNativeAdConfiguration mediationNativeAdConfiguration;
+
+    /**
+     * Callback that fires on loading success or failure.
+     */
+    private final MediationAdLoadCallback<com.google.android.gms.ads.mediation.NativeAdMapper, MediationNativeAdCallback>
+            mediationAdLoadCallback;
+
+    /**
+     * Tag used for log statements
+     */
+    private static final String TAG = "NativeCustomEvent";
+
+    /**
+     * Callback for native ad events. The usual link/click tracking handled through callback methods
+     * are handled through the GMA SDK, described here:
+     * https://developers.google.com/admob/android/custom-events/native#impression_and_click_events
+     */
+    @SuppressWarnings("unused")
+    private MediationNativeAdCallback mediationNativeAdCallback;
+
+    public NativeCustomEventLoader(
+            @NonNull MediationNativeAdConfiguration mediationNativeAdConfiguration,
+            @NonNull MediationAdLoadCallback<com.google.android.gms.ads.mediation.NativeAdMapper, MediationNativeAdCallback>
+                    mediationAdLoadCallback) {
+        this.mediationNativeAdConfiguration = mediationNativeAdConfiguration;
+        this.mediationAdLoadCallback = mediationAdLoadCallback;
+    }
+
+    /**
+     * Loads the native ad from the third party ad network.
+     */
+    public void loadAd() {
+        // All custom events have a server parameter named "parameter" that returns back the parameter
+        // entered into the AdMob UI when defining the custom event.
+        String serverParameter = mediationNativeAdConfiguration.getServerParameters().getString("parameter");
+        if (TextUtils.isEmpty(serverParameter)) {
+            mediationAdLoadCallback.onFailure(CustomEventError.createCustomEventNoAdIdError());
+            return;
+        }
+        Log.d(TAG, "Received server parameter.");
+
+        Context context = mediationNativeAdConfiguration.getContext();
+
+        //log event to firebase
+        FirebaseAnalyticsUtil.logEventMediationAdx(context, FirebaseAnalyticsUtil.NATIVE);
+
+        AdLoader.Builder builder = new AdLoader.Builder(context, serverParameter);
+        // OnLoadedListener implementation.
+        builder.forNativeAd(nativeAd -> {
+            NativeAdMapper mappedAd = new NativeAdMapper(nativeAd, context);
+            mediationNativeAdCallback = mediationAdLoadCallback.onSuccess(mappedAd);
+            mappedAd.setMediationNativeAdCallback(mediationNativeAdCallback);
+        });
+
+        VideoOptions videoOptions =
+                new VideoOptions.Builder().setStartMuted(true).build();
+
+        NativeAdOptions adOptions = new NativeAdOptions.Builder().setVideoOptions(videoOptions).build();
+
+        builder.withNativeAdOptions(adOptions);
+
+        AdLoader adLoader = builder.withAdListener(new AdListener() {
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                super.onAdFailedToLoad(loadAdError);
+                mediationAdLoadCallback.onFailure(new AdError(loadAdError.getCode(), loadAdError.getMessage(), loadAdError.getDomain()));
+            }
+
+            @Override
+            public void onAdImpression() {
+                super.onAdImpression();
+                if (mediationNativeAdCallback != null)
+                    mediationNativeAdCallback.reportAdImpression();
+            }
+
+            @Override
+            public void onAdClicked() {
+                super.onAdClicked();
+                if (mediationNativeAdCallback != null)
+                    mediationNativeAdCallback.reportAdClicked();
+            }
+
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                if (mediationNativeAdCallback != null)
+                    mediationNativeAdCallback.onAdClosed();
+            }
+
+            @Override
+            public void onAdOpened() {
+                super.onAdOpened();
+                if (mediationNativeAdCallback != null)
+                    mediationNativeAdCallback.onAdOpened();
+            }
+        }).build();
+        adLoader.loadAd(new AdRequest.Builder().build());
+    }
+}
